8595710a7dad7fcb66caa57eea84e248ab0aaf4c
brylimz <superuser@brylimz.com>
2022-01-15 09:36:10 +0700

    i need rest

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
 from flask import Flask
+import random
+ # set FLASK_APP=hello.py
+ # $env:FLASK_APP = "hello.py"
+ # flask run
 
 app = Flask(__name__)
+print(__name__)
+print(random.__name__)
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    app.run()
